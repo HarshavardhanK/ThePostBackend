9e6db4fbfd97ea48ae4438f838cbc43152ab48c7
HarshavardhanK <harshavardhan816@gmail.com>
2019-07-09 01:53:12 +0530

    added nginx config bash script

--- nginx-config.sh
+++ nginx-config.sh
@@ -1,22 +1,5 @@
 #!/usr/bin/env bash
 
-#INSTALL NODEJS
-
-sudo apt-get update
-sudo apt-get install nodejs
-sudo apt-get install npm
-
-#INSTALL MONGODB
-sudo apt update
-sudo apt install -y mongodb
-
-#check the service and database
-mongo --eval 'db.runCommand({ connectionStatus: 1 })'
-#if not working ABORT
-
-sudo systemctl enable mongodb
-sudo systemctl start mongodb
-
 #INSTALL NGINGX
 sudo apt update
 sodu apt install nginx
@@ -30,7 +13,3 @@ sudo systemctl enable nginx
 sudo nano /etc/nginx/sites-available/api.themitpost.com
 cp -i /root/ThePostBackend/api.themitpost.com /etc/nginx/sites-available/api.themitpost.com
 sudo ln -s /etc/nginx/sites-available/api.themitpost.com /etc/nginx/sites-enabled/
-
-#FIREWALL
-ufw app list
-ufw allow OpenSSH

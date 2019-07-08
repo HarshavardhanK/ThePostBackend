85957732b6a318f5205c79ee84910f1cc4c12954
Harshavardhan K <harshavardhan816@gmail.com>
2019-07-09 01:08:14 +0530

    added shell scripting

---
+++ server-config.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+#INSTALL NODEJS
+
+sudo apt-get update
+sudo apt-get install nodejs
+sudo apt-get install npm
+
+#INSTALL MONGODB
+sudo apt update
+sudo apt install -y mongodb
+
+#check the service and database
+mongo --eval 'db.runCommand({ connectionStatus: 1 })'
+#if not working ABORT
+
+sudo systemctl enable mongodb
+sudo systemctl start mongodb
+
+#FIREWALL
+ufw app list
+ufw allow OpenSSH
+
+
+#MAKE DIRECTORY TO CONTAIN YOUR SCRIPTS
+mkdir The-Post
+cd The-Post/

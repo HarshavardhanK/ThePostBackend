7e30891e2f19003852379f0697e0f53f6252bf9d
Harshavardhan K <harshavardhan816@gmail.com>
2019-08-31 21:46:21 +0530

    added better yargs to update.js

--- SupermanSLCM/update.js
+++ SupermanSLCM/update.js
@@ -30,7 +30,7 @@ const url = "mongodb://localhost:27017/";
 
 const fetch = async (registration, password, test) => {
 
-  let URL = 'https://localhost:8000/values/update';
+  let URL = 'https://api.themitpost.com/values/update';
 
   if(test) {
     URL = 'http://localhost:3000/values/update'

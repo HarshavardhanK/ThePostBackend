c61758bc6fbf0395f36a4ebb6e8213cbd2492f06
Harshavardhan K <harshavardhan816@gmail.com>
2019-09-02 21:23:38 +0530

    added shell scripts for cron job

--- cron-update.sh
+++ cron-update.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo 'This script runs update.js on 8am, 9am, 10am, 11am, 12am, 1pm, 2pm, 3pm, 4pm, 5pm, 6pm, 7pm, 8pm, 9pm, 10pm, 11pm every day of every week'
+echo 'This script runs update.js on 2am - 6pm UTC every day of every week'
 pm2 stop all
 
 cd SupermanSLCM/
@@ -11,3 +11,5 @@ cd ..
 pm2 start server.js --max-memory-restart 200M -i max
 
 echo 'Scripts running succesfully'
+
+# 00 2-18 * * * ~/ThePostBackend/cron-update.sh

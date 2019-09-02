138be2217e7e3d31fa8a57e1875bd9850bb935c6
Harshavardhan K <harshavardhan816@gmail.com>
2019-09-02 21:47:42 +0530

    added PATH for cron to work

--- cron-update.sh
+++ cron-update.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-echo 'This script runs update.js on 2am - 6pm UTC every day of every week'
+/bin/echo 'This script runs update.js on 2am - 6pm UTC every day of every week'
 pm2 stop all
 
-cd SupermanSLCM/
-pm2 start update.js --max-memory-restart 200M -i max
+/bin/cd SupermanSLCM/
+/bin/pm2 start update.js --max-memory-restart 200M -i max
 
-cd ..
+/bin/cd ..
 
-pm2 start server.js --max-memory-restart 200M -i max
+/bin/pm2 start server.js --max-memory-restart 200M -i max
 
-echo 'Scripts running succesfully'
+/bin/echo 'Scripts running succesfully'
 
 # 34 2-18 * * * /root/ThePostBackend/cron-update

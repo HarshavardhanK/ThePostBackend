8a1c5ddb8aec95c745eb8550853b2514e8ce90fc
Harshavardhan K <harshavardhan816@gmail.com>
2019-09-02 20:30:15 +0530

    changed memory and made it dynamic from input

--- start_server.sh
+++ start_server.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-pm2 start server.js --max-memory-restart 250M -i max
+pm2 start server.js --max-memory-restart $1 -i max
 cd SupermanSLCM/
 
-pm2 start update.js --max-memory-restart 400M -i max
+pm2 start update.js --max-memory-restart $2 -i max
 cd ~/ThePostBackend/ 
 
-echo 'Successfully started server.js and update.js daemon in cluster mode'
+echo 'Successfully started server.js in $1 and update.js $2 daemon in cluster mode'

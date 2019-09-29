01e422e97b3d877a9c919c6d820bcea23f175ee7
Rakshit GL <rakshitgl@live.com>
2019-09-29 16:22:26 +0530

    Branch trackers GTFO

--- Notices/index.js
+++ Notices/index.js
@@ -1,7 +1,7 @@
-
+const express = require('express');
 const path = require('path');
 const FCM = require('fcm-node');
-
+const app = express();
 
 //Required to authenticate to FCM
 const server_key = "AAAAG-SAMsE:APA91bHO8utPUH9yrRyJA37KlCSUiUg44NdyQicqJCbdk0f5oJ9eh4Rfew8uuGsnEyd_EZtcanzB7ecqL6E83wOZqfm_B3KSo4Kw1mu_8phDZJ5HM6i9TPX6k-gjBmmJquWU1zh82fKT";
@@ -13,23 +13,19 @@ var fcm = new FCM(server_key);
 
 const database = require('./database.js');
 
-//RAKSHIT USE THE FUNCTION BELOW FOR YOUR PURPOSES I SUPPOSE
+app.get('/',function(req,res){
+  res.sendFile(path.join(__dirname+'/static/initial.html'));
+});
 
-module.exports.get_base_for_notice = () => {
+const bodyParser = require('body-parser');
 
-  const express = require('express');
-  const notice_app = express();
 
-  const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-  notice_app.use(bodyParser.urlencoded({ extended: false }));
-  notice_app.use(bodyParser.json());
 
-  notice_app.get('/',function(req,res){
-    res.sendFile(path.join(__dirname+'/static/initial.html'));
-  });
+app.post('/portal/notices/submitted', function(request, response) {
 
-<<<<<<< HEAD
   console.log(request.body);
 
   const title = request.body.title;
@@ -38,12 +34,10 @@ module.exports.get_base_for_notice = () => {
   const time = request.body.time;
   const pdfLink = request.body.pdfLink;
   const imageLink = request.body.imageURL;
-=======
-}
->>>>>>> 2b93ea1088ae0b5249f37a9fb71edfd7034b7eea
 
+  //Grab the value of the checkbox "notify?"
+  const notify = request.body.notify;
 
-<<<<<<< HEAD
   const notice = {
     title: title,
     content: content,
@@ -73,14 +67,9 @@ module.exports.get_base_for_notice = () => {
   console.log(notify);
 
   if(notify == 'on'){
-=======
 
-module.exports.notices_post = (app) => {
->>>>>>> 2b93ea1088ae0b5249f37a9fb71edfd7034b7eea
+    //If notify checkbox is checked then send notification
 
-  app.post('/portal/notices/submitted', function(request, response) {
-
-<<<<<<< HEAD
     console.log("Attempting to send notification");
 
     fcm.send(message, function(err, response){
@@ -94,84 +83,24 @@ module.exports.notices_post = (app) => {
   }
 
   database.insert_notice(notice);
-=======
-    const title = request.body.title;
-    const content = request.body.content;
-    const date = request.body.date;
-    const time = request.body.time;
-  
-    //Grab the value of the checkbox "notify?"
-    const notify = request.body.notify;
-  
-    var message = {
-      to: topic,
-      notification: {
-        title: title,
-        body:content
-      }
-    };
-  
-    if(notify === 'on'){
-  
-      //If notify checkbox is checked then send notification
-  
-      fcm.send(message, function(err, response){
-        if (err) {
-            console.log(err);
-        }
-        else {
-            console.log("Successfully sent with response: ", response);
-        }
-      });
+
+  response.send('<h1>This notice has been added. Thank you!</h1><p>&copy The MIT Post 2019. All rights reserved.</p>');
+
+});
+
+app.get('/notices', function(request, response) {
+
+  database.get_notices_all(result => {
+
+    if(result) {
+      console.log(result);
+      result = {status: "OK", data: result};
+
+      response.json(result);
     }
-  
-    const imagesTemp = request.body.imageURL;
-  
-    var images = [];
-  
-    for(var i=0;i<imagesTemp.length;i++) {
-  
-      var element = imagesTemp[i];
-  
-      if(element != null && element != '')
-        images.push(element);
-    }
-  
-    const numImages = images.length;
-  
-    const notice = {
-      title: title,
-      content: content,
-      date: date,
-      time: time,
-      numImages:numImages,
-      images:images
-    };
-  
-    database.insert_notice(notice);
-  
-    response.send('<h1>This notice has been added. Thank you!</h1><p>&copy The MIT Post 2019. All rights reserved.</p>');
-  
+
   });
 
-}
+});
 
->>>>>>> 2b93ea1088ae0b5249f37a9fb71edfd7034b7eea
-
-
-
-// app.get('/notices', function(request, response) {
-
-//   database.get_notices_all(result => {
-
-//     if(result) {
-//       console.log(result);
-//       result = {status: "OK", data: result};
-
-//       response.json(result);
-//     }
-
-//   });
-
-// });
-
+app.listen(8080);
